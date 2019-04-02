9f81d527220238196f1309666a77139b427f9d94
RyanAragao2 <47668352+RyanAragao2@users.noreply.github.com>
2019-04-02 11:13:22 -0300

    Add files via upload

---
+++ main.py.py
@@ -0,0 +1,70 @@
+from bs4 import BeautifulSoup
+from clint.textui import colored
+import requests
+
+print(colored.red("Exemplo: https://site.com/"))
+print(colored.red("Exemplo2: http://site.com/"))
+url = input("Digite o site: ")
+
+header = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                        'AppleWebKit/537.36 (KHTML, like Gecko) '
+                        'Chrome/51.0.2704.103 Safari/537.36'}
+
+reque = requests.get(url, headers=header)
+html = reque.text
+sopinha = BeautifulSoup(html, 'html.parser')
+
+
+def opcao():
+    banner = """
+    +------------------------------------------------+
+    |                    HTINFO                      |
+    | Ferramenta para colher dados pelo código fonte |
+    |           Criada por: Ryan Aragão              |
+    |        https://github.com/RyanAragao2          |
+    +------------------------------------------------+
+    """
+    print(colored.yellow(banner))
+    print("0- Sair da Ferramenta")
+    print("1- Procurar Imagens")
+    print("2- Procurar Títulos")
+    print("3- Pegar Todos os Links do site")
+
+
+opcao()
+while True:
+    opcaozinha = int(input("Digite o número da opção: "))
+    if opcaozinha == 0:
+        print("Até mais!")
+        break
+    elif opcaozinha == 1:
+        imagem = sopinha.find_all('img')
+        for i in imagem:
+            print(i.get('src'))
+    elif opcaozinha == 2:
+        titulo = sopinha.find('title')
+        for t in titulo:
+            print(titulo)
+    elif opcaozinha == 3:
+        links = sopinha.find_all('a')
+        for l in links:
+            print(l.get('href'))
+    else:
+        print("Essa opção não existe")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
